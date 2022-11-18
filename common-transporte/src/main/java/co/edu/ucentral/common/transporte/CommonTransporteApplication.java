298a54c64384feccd1493089d07edd78c3c6169c
stev25pr <stevenp2001@gmail.com>
2022-11-18 12:56:27 -0500

    prueba

---
+++ common-transporte/src/main/java/co/edu/ucentral/common/transporte/CommonTransporteApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.common.transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonTransporteApplication.class, args);
+	}
+
+}
---
+++ common-transporte/src/test/java/co/edu/ucentral/common/transporte/CommonTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.common.transporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
